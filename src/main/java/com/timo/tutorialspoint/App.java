90bbb693dbd5ae9e6b2d95487603467f92e8e8a4
Timo Wang <chiuanw@yahoo-inc.com>
2018-07-17 16:22:48 +0800

    provides binding

--- src/main/java/com/timo/tutorialspoint/App.java
+++ src/main/java/com/timo/tutorialspoint/App.java
@@ -5,6 +5,7 @@ import com.google.inject.BindingAnnotation;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.Provides;
 import com.google.inject.name.Names;
 import com.google.inject.name.Named;
 
@@ -24,8 +25,24 @@ interface SpellChecker {
 }
 
 class SpellCheckerImpl implements SpellChecker {
+    private String dbUrl;
+    private String user;
+    private Integer timeout;
+
+    public SpellCheckerImpl() {
+    }
+
+    public SpellCheckerImpl(String dbUrl, String user, Integer timeout) {
+        this.dbUrl = dbUrl;
+        this.user = user;
+        this.timeout = timeout;
+    }
+
     public void checkSpelling() {
         System.out.println("Inside checkSpelling.");
+        System.out.println(dbUrl);
+        System.out.println(user);
+        System.out.println(timeout);
     }
 }
 
@@ -33,27 +50,28 @@ class SpellCheckerImpl implements SpellChecker {
 class TextEditorModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(SpellChecker.class).annotatedWith(Names.named("OpenOffice")).to(WinWordSpellCheckerImpl.class);
-        bind(String.class).annotatedWith(Names.named("JDBC")).toInstance("jdbc:mysql://localhost:5326/emp");
+    }
+
+    @Provides
+    public SpellChecker provideSpellChecker() {
+        String dbUrl = "jdbc:mysql://localhost:5326/emp";
+        String user = "user";
+        int timeout = 100;
+        SpellChecker spellChecker = new SpellCheckerImpl(dbUrl, user, timeout);
+        return spellChecker;
     }
 }
 
 class TextEditor {
     private SpellChecker spellChecker;
-    private String dbUrl;
 
     @Inject
-    public TextEditor(@Named("OpenOffice") SpellChecker spellChecker, @Named("JDBC") String dbUrl) {
+    public TextEditor(SpellChecker spellChecker) {
         this.spellChecker = spellChecker;
-        this.dbUrl = dbUrl;
     }
 
     public void makeSpellCheck() {
         spellChecker.checkSpelling();
-    }
-
-    public void makeConnection() {
-        System.out.println(dbUrl);
     }
 }
 
@@ -85,6 +103,5 @@ public class App
         Injector injector = Guice.createInjector(new TextEditorModule());
         TextEditor editor = injector.getInstance(TextEditor.class);
         editor.makeSpellCheck();
-        editor.makeConnection();
     }
 }
