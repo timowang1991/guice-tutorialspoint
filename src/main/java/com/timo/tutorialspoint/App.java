51bed4e33555d507224bf33b85743e9f2141d4ec
Timo Wang <chiuanw@yahoo-inc.com>
2018-07-17 15:45:55 +0800

    add with Named annotation

--- src/main/java/com/timo/tutorialspoint/App.java
+++ src/main/java/com/timo/tutorialspoint/App.java
@@ -5,6 +5,8 @@ import com.google.inject.BindingAnnotation;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.name.Names;
+import com.google.inject.name.Named;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -31,7 +33,7 @@ class SpellCheckerImpl implements SpellChecker {
 class TextEditorModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(SpellChecker.class).annotatedWith(WinWord.class).to(WinWordSpellCheckerImpl.class);
+        bind(SpellChecker.class).annotatedWith(Names.named("OpenOffice")).to(OpenOfficeWordSpellCheckerImpl.class);
     }
 }
 
@@ -39,7 +41,7 @@ class TextEditor {
     private SpellChecker spellChecker;
 
     @Inject
-    public TextEditor(@WinWord SpellChecker spellChecker) {
+    public TextEditor(@Named("OpenOffice") SpellChecker spellChecker) {
         this.spellChecker = spellChecker;
     }
 
@@ -52,6 +54,16 @@ class WinWordSpellCheckerImpl extends SpellCheckerImpl {
     @Override
     public void checkSpelling() {
         System.out.println("Inside WinWordSpellCheckerImpl.checkSpelling.");
+    }
+}
+
+/**
+ * OpenOfficeWordSpellCheckerImpl
+ */
+class OpenOfficeWordSpellCheckerImpl extends SpellCheckerImpl {
+    @Override
+    public void checkSpelling() {
+        System.out.println("Inside OpenOfficeWordSpellCheckerImpl.checkSpelling.");
     }
 }
 
